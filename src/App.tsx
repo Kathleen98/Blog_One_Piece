c93e47efcd359dc73ecb630979a3d604e3852f18
Kathleen Santos <kathleen.santos@csacademy.com.br>
2024-04-28 09:39:52 -0300

    Setup do projeto

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import '@mantine/core/styles.css';
+import HeroBanner from '../components/Banner/HeroBanner';
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <HeroBanner/>
+      </div>
+    </>
+  )
+}
+
+export default App
